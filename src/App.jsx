9f2331d51c78b89b4d51dd2b54cbb209564de83c
dhakiweere <dhanikaweerasekara@outlook.com>
2025-10-29 02:49:54 +0530

    about section | scroll issues | ProjectListItem Comp add

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import logo from './assets/logo.png'
 import gh_logo from './assets/github_logo.png'
 import lnk_logo from './assets/linkedin_logo2.png'
 import { context, titleChangeJob } from './utils/titleAnimate'
+import ProjectListItem from './component/ProjectListItem'
 
 // DEBUG
 // import './debug.css'
@@ -26,20 +27,25 @@ export default function () {
 
 
   return (
-    <div className='container-top w-full h-[200vh] flex flex-col justify-center items-center'>
+    <div className='container-top scroll-smooth w-full h-screen overflow-y-scroll flex flex-row justify-center items-start'>
 
       {/* Container actual */}
-      <div className="container-actual bg-transparent w-full max-w-6xl h-full 
+      <div className="container-actual bg-transparent w-full max-w-6xl h-fit 
           flex flex-col items-start gap-y-10 xl:gap-y-20"
       >
 
         {/* HEADER */}
-        <div className='bg-linear-to-r from-dark-main font-mono to-transparent max-h-[20%] w-full
+        <div className='bg-linear-to-r from-dark-main font-mono to-transparent max-h-[20vh] w-full
          flex flex-row gap-x-2 
+         sticky top-0
          border-b-2 border-s-10 xl:border-s-20 ps-5 p-2'>
           <img className='logo h-8' src={logo} />
           <div className='flex-1'></div>
-          <p className='flex items-center font-bitcount'>About</p>
+          <a  href='#about' className='flex items-center font-bitcount 
+          hover:scale-110 hover:cursor-pointer hover:underline
+          '>
+            About
+          </a>
 
         </div>
         {/* HEADER */}
@@ -57,34 +63,74 @@ export default function () {
         {/* TITLE */}
 
         {/* PROJECTS */}
-        <div className='animate-border h-fit xl:min-h-[200px] w-full flex flex-col gap-y-3 p-3'>
-          <p className='w-fit text-4xl xl:text-5xl mb-3 font-bitcount border-dotted border-b-2'>Projects</p>
-          <ol className='text-xl font-inconsolata'>
-            {/* reflex ci/cd */}
-            <li >
-              <div className='animate-selection mb-3 flex flex-row gap-x-2 items-center justify-baseline'>
-                <p className="font-bold w-[15ch]">Reflex Ci/Cd</p>
-                <div className='hidden md:block w-40'></div>
-                <p className='hidden xl:block italic text-left'>Self reacting duplex Ci/CD Pipeline</p>
-                <div className='flex-1'></div>
-                <a href='http://reflex.dhakiweere.space' className='animate-button-online'>Online</a>
-                <a href='https://github.com/dhakiweere/reflex-ci-cd' className='animate-button-repo'>Repo</a>
-              </div>
-            </li>
+        <div className='animate-border h-fitb xl:min-h-[200px] w-full flex flex-col gap-y-3 p-3'> <p className='w-fit text-4xl xl:text-5xl mb-3 font-bitcount border-dotted border-b-2'>Projects</p> <ol className='text-xl font-inconsolata'>
 
-            {/* other one */}
-            <li>
-              <div className='animate-selection mb-3 flex flex-row gap-x-2 items-center justify-baseline'>
-                <p className="font-bold w-[15ch]" >Thock Simulator</p>
-                <div className='hidden md:block w-40'></div>
-                <p className='hidden xl:block italic text-left'>Mechanical key input sound on Console</p>
-                <div className='flex-1'></div>
-                <a href='http://reflex.dhakiweere.space' className='animate-button-repo'>Repo</a>
-              </div>
-            </li>
-          </ol>
+          {/* reflex ci/cd */}
+          <li>
+            <ProjectListItem
+              title="Reflex Ci/Cd"
+              desc="Self reacting duplex Ci/CD Pipeline"
+              onlineURL="http://reflex.dhakiweere.space"
+              repoURL="https://github.com/dhakiweere/reflex-ci-cd"
+            />
+          </li>
+
+          {/* lan chat*/}
+          <li>
+            <ProjectListItem
+              title="Lan Chat"
+              desc="Console-Based Chat Application for Local Area Networks"
+              repoURL="https://github.com/dhakiweere/lan-chat"
+            />
+          </li>
+
+          {/* master observer*/}
+          <li>
+            <ProjectListItem
+              title="Master Observer"
+              desc="File System Monitor with Real-Time Change Detection"
+              repoURL="https://github.com/dhakiweere/master-observer"
+            />
+          </li>
+
+          {/* auto loader*/}
+          <li>
+            <ProjectListItem
+              title="Auto Loader"
+              desc="JavaScript Plugin that enables auto refreshing Web Pages in development time."
+              repoURL="https://github.com/dhakiweere/auto-loader"
+            />
+          </li>
+
+        </ol>
         </div>
         {/* PROJECTS */}
+
+        {/* ABOUT*/}
+        <div id='about' className='animate-border h-fit w-full flex flex-col gap-y-3 p-3'>
+          <p className='w-fit text-4xl xl:text-5xl mb-3 font-bitcount border-dotted border-b-2'>About</p>
+          <p className='w-[35ch] xl:w-[100ch] font-inconsolata italic text-xl '>
+            I am a final-year undergraduate student at the Java Institute of Advanced Technology,
+            pursuing a degree in information technology. <br /><br /> I have a strong passion for cryptography, DevOps, and
+            cloud computing, with a particular interest in building secure, scalable, and automated systems.
+            <br /><br />I aim to become a skilled Cloud/DevOps Engineer, contributing to innovative and reliable infrastructure solutions.
+            Outside of my academic and technical pursuits, I enjoy swimming and open skies.
+          </p>
+        </div>
+        {/* ABOUT*/}
+
+        {/* FOOTER */}
+        <footer className="footer w-full h-fit
+          flex flex-row justify-center items-center
+        ">
+          <div className='bg-linear-to-b from-dark-main from-40% to-transparent rounded-t-xl
+           font-inconsolata text-sm text-center p-2 font-bold
+           flex flex-col md:flex-row'>
+            <p>&copy; 2025 Dhanika Weerasekara &nbsp;</p>
+            <p>All rights reserved</p>
+          </div>
+        </footer>
+        {/* FOOTER */}
       </div>
     </div>
   )
